5a6de7261b0942a7e62433bedee59bb49e92d7e1
buddyJuan <juandcg4@gmail.com>
2020-11-27 23:26:08 -0800

    BreadCrumbs

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 function RenderDirectoryItem({campsite}){
     return (
@@ -16,20 +16,30 @@ function RenderDirectoryItem({campsite}){
 }
 
  function Directory(props) {
-     const directory = props.campsites.map(campsite => {
-            return ( 
-                <div key={campsite.id} className="col-md-5 m-1">
-                  <RenderDirectoryItem campsite={campsite} />
-                </div>
-            );
-        });
-        return ( 
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
-            </div>
-        );
-}
+   const directory = props.campsites.map((campsite) => {
+     return (
+       <div key={campsite.id} className="col-md-5 m-1">
+         <RenderDirectoryItem campsite={campsite} />
+       </div>
+     );
+   });
+   return (
+     <div className="container">
+       <div className="row">
+         <div className="col">
+           <Breadcrumb>
+             <BreadcrumbItem>
+               <Link to="/home">Home</Link>
+             </BreadcrumbItem>
+             <BreadcrumbItem active>Directory</BreadcrumbItem>
+           </Breadcrumb>
+           <h2>Directory</h2>
+           <hr />
+         </div>
+       </div>
+       <div className="row">{directory}</div>
+     </div>
+   );
+ }
 
 export default Directory; 
