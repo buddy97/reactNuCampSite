3bcd4ee9de3cbb6408b9b853748bd10ed92b3765
buddyJuan <juandcg4@gmail.com>
2020-11-27 23:12:16 -0800

    React Router Parameters

--- src/components/Main.js
+++ src/components/Main.js
@@ -23,7 +23,6 @@ class Main extends Component {
       promotions: PROMOTIONS
     };
   }
-
   render(){
     const HomePage = () => {
       return (
@@ -33,7 +32,16 @@ class Main extends Component {
           partner={this.state.partners.filter(partner => partner.featured)[0]}
         />
       );
-    };
+  };
+    const CampsiteWithId = ({match}) => {
+    return (
+      <CampsiteInfo 
+        campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+        comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+        />
+      );
+    }; 
+  
     return (
       <div>
         <Header />
@@ -41,6 +49,7 @@ class Main extends Component {
           <Route path='/home' component={HomePage}/>
           <Route exact path='/directory' render={() => <Directory campsites = {this.state.campsites} />} />
           <Route exact path='/contactus' component={Contact}/>
+          <Route path='/directory/:campsiteId' component={CampsiteWithId} />
           <Redirect to='/home'/>
         </Switch>
         <Footer />
