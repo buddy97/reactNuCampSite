7e1385da391df2671689f1df8210f64a1ce6d04a
buddyJuan <juandcg4@gmail.com>
2020-12-11 21:59:37 -0800

    React Redux Forms Revisited

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,6 +9,7 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Contact from './Contact';    
 import { connect } from "react-redux";
 import { addComment, fetchCampsites } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = (state) => {
   return {
@@ -21,7 +22,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = {
   addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
-  fetchCampsites:() => (fetchCampsites())
+  fetchCampsites:() => (fetchCampsites()),
+  resetFeedbackForm: () => (actions.reset('feedbackForm'))
 };
 
 class Main extends Component {
@@ -58,12 +60,26 @@ class Main extends Component {
       <div>
         <Header />
         <Switch>
-          <Route path='/home' component={HomePage}/>
-          <Route exact path='/directory' render={() => <Directory campsites = {this.props.campsites} />} />
-          <Route exact path='/aboutus' render={() => <About partners = {this.props.partners} />} />
-          <Route exact path='/contactus' component={Contact}/>
-          <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-          <Redirect to='/home'/>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/directory"
+            render={() => <Directory campsites={this.props.campsites} />}
+          />
+          <Route
+            exact
+            path="/aboutus"
+            render={() => <About partners={this.props.partners} />}
+          />
+          <Route
+            exact
+            path="/contactus"
+            render={() => (
+              <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+            )}
+          />
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
+          <Redirect to="/home" />
         </Switch>
         <Footer />
       </div>
